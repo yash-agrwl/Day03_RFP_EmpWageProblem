f94815e88a4957d3eff81f1e110f545768e9e429
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-11 13:20:05 +0530

    added uc1: Check if employee is Present or Absent

---
+++ EmployeeWage/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Employee
+    {
+        const int Is_Present = 1;
+        public static int Check()
+        {
+            Random random = new Random();
+            return random.Next(0, 2);
+        }
+        public static void Attendance()
+        {
+            int empCheck = Employee.Check();
+            if (empCheck == Is_Present)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,7 +6,8 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Employee Wage Computation Program.\n");
+            Employee.Attendance();
         }
     }
 }
