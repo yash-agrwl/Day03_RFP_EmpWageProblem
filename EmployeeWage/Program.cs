3515e7ad00ac76fee809f5d6fee04fd03471cad7
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-11 11:33:32 +0530

    added empty project with welcome message

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
