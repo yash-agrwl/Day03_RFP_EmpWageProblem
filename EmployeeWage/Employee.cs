5e2ef54922f118e1c744190470a819f9e2619bbc
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-11 15:43:21 +0530

    added uc3: Part Time Employee & Wage

--- EmployeeWage/Employee.cs
+++ EmployeeWage/Employee.cs
@@ -10,25 +10,31 @@ namespace EmployeeWage
     {
         const int IsAbsent = 0;
         const int IsFullTime = 1;
+        const int IsPartTime = 2;
         const int WagePerHr = 20;
         const int FullTimeHr = 8;
+        const int PartTimeHr = 4;
         public static int Check()
         {
             Random random = new Random();
-            return random.Next(0, 2);
+            return random.Next(0, 3);
         }
         public static string Attendance(int empCheck)
         {
-            if (empCheck == IsAbsent)
-                return "Absent";
+            if (empCheck == IsFullTime)
+                return "Present and Full Time";
+            else if (empCheck == IsPartTime)
+                return "Present but Part Time";
             else
-                return "Present";          
+                return "Absent";
         }
         public static int DailyEmpWage(int empCheck)
         {
             int empHrs;
             if (empCheck == IsFullTime)
                 empHrs = FullTimeHr;
+            else if (empCheck == IsPartTime)
+                empHrs = PartTimeHr;
             else
                 empHrs = 0;
             int empWage = empHrs * Employee.WagePerHr;
